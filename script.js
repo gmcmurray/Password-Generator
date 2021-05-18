22022f33a1817d7657775fb5e3bcf960c474cf90
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-05-17 17:36:22 -0700

    Added checkers function for input error in js. Checks for length and makes sure at least one group of chars is selected.  Added reset function clearing inputs (clearinputs())

--- script.js
+++ script.js
@@ -1,63 +1,65 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-function generatepwd() {
-if(window.confirm("Proceed to Instructions by clicking OK"))
-{ document.querySelector("#password").value = "Instructions: 1) Set desired length of password 2) pick comp"}
-else{window.alert("Okay thanks for considering!")}
-};
-
 // Globals and helper functions
 // Four sets of characters for password
 var lowercase = "qwertyuiopasdfghjklzxcvbnm";
 var uppercase=lowercase.toUpperCase();
-var numerics="0123456789"
-var specialchars="~!(-_][.>;<:@%&*+="
-// userinfo collected in passwordinfo Object.
+var numerics="0123456789";
+var specialchars="~!(-_][.>;<:@%&*+=";
+var password="";
+var buttons=["lwcase","upcase","number","spchar"]
 
-// Function to query user for password info
+// password object with five user inputs and one good password checker
 var passwordcom = {
   lowerc: false,
-  upperc: true,
+  upperc: false,
   numb: false,
-  spechar: true,
+  spechar: false,
   length:8,
   good:false,
 };
-function passwdcomp(){
-    passwordcom.lowerc = document.getElementById("lwcase").checked;
-    passwordcom.upperc = document.getElementById("upcase").checked;
-    passwordcom.numb = document.getElementById("number").checked;
-    passwordcom.spechar = document.getElementById("spchar").checked;
-    passwordcom.length = parseInt(document.getElementById("passwdlength").value);
-    passwordcom.good=window.confirm("Are your selections?  " + "lowercase " + passwordcom.lowerc +
-    " uppercase " + passwordcom.upperc + " number " +  passwordcom.numb + 
-     "specialchar " + passwordcom.spechar + "length of password " + passwordcom.length)
-    // passwordcom.good=cfrm();
-    console.log("good ", passwordcom);
-    errorchk();
-    };
 
-    var errorchk = function(){
-      if(passwordcom.length>128 || passwordcom.lenth<8 || isNaN(passwordcom.lenth))
-   {window.alert("Error,Make sure to type in number between 8 and 128")}
-    };
-  
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Error check function for password length and missing inputs 
 
-var usersetfun = function(){
+var errorchk = function(){
+  if(passwordcom.length>128 || passwordcom.length<8 || isNaN(passwordcom.length)||(
+  !passwordcom.lowerc && !passwordcom.upperc && !passwordcom.numb && !passwordcom.spechar))
+  { 
+    window.alert("Error,Make sure to type in number between 8 and 128, and have at least one char group selection, Press Generate Password again");
+    clearinputs();}
+  return;
+};
+
+var clearinputs = function(){
+  for (var x in passwordcom) {
+    passwordcom[x]="false";
+  };
+  // var inputs = document.getElementsByClassName("ckboxes");
+  // console.log("inputs" , inputs);
+  // for (var i = 0; i < inputs.length; i++) {
+          // inputs[i].checked = false;};
+  // document.getElementsByClassName("ckboxes").checked=false;
+  document.getElementById("lwcase").checked=false;
+  for (var x=0; x < buttons.length;x++){
+    document.getElementById(buttons[x]).checked=false;
+  }
+  console.log("lwcase",document.getElementById("lwcase").checked)
+  document.getElementById("passwdlength").value="";
+  return;
+};
+// Character set generator (userset) based on user inputs - passwordcom
+function usersetfun(){
   document.querySelector("#generate");
-  userset="";
-  if(passwordcom.upperc){userset=userset+uppercase}
-  if(passwordcom.lowerc){userset=userset+lowercase}
-  if(passwordcom.numb){userset=userset+numerics}
-  if(passwordcom.spechar){userset=userset+specialchars}
-  return(userset)
-}
+  var userset="";
+  if(passwordcom.upperc){userset=userset+uppercase};
+  if(passwordcom.lowerc){userset=userset+lowercase};
+  if(passwordcom.numb){userset=userset+numerics};
+  if(passwordcom.spechar){userset=userset+specialchars};
+  return(userset);
+};
 
-console.log(passwordcom)
-var Userset= usersetfun();
-console.log(Userset);
+// Random selector of characters from userset 
 
 var passwordresult = function(userset){
   var passwd="";
@@ -67,14 +69,42 @@ var passwordresult = function(userset){
   return(passwd)
 };
 
-var password=passwordresult(Userset);
-console.log(password);
-console.log(typeof(password));
-
 function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+  return;
 }
 writePassword();
 // Add event listener to generate button
 generateBtn.addEventListener("click",writePassword());
+
+function passwdcomp(){
+    passwordcom.lowerc = document.getElementById("lwcase").checked;
+    passwordcom.upperc = document.getElementById("upcase").checked;
+    passwordcom.numb = document.getElementById("number").checked;
+    passwordcom.spechar = document.getElementById("spchar").checked;
+    passwordcom.length = parseInt(document.getElementById("passwdlength").value);
+    passwordcom.good = confirm("Are your selections?  " + "lowercase " + passwordcom.lowerc + "; uppercase " + passwordcom.upperc + "; number " +  passwordcom.numb + "; specialchar " + passwordcom.spechar + "; length of password " + passwordcom.length);
+    // passwordcom.good=cfrm();
+    console.log("good ", passwordcom);
+    passwordcom.good=errorchk();
+    console.log(passwordcom);
+    if(3){
+      var Userset = usersetfun();
+      password=passwordresult(Userset);
+      writePassword();
+      return(password);
+    }
+    else{window.alert("There has been an error-press Generate Password again")}
+    console.log(passwordcom)
+    };
+
+// Main function enabled by Generate Password button
+    function generatepwd() {
+      if(window.confirm("Proceed to Instructions by clicking OK"))
+      { document.querySelector("#password").value = "Instructions: 1) Set desired length of password 2) pick comp";
+        //document.querySelectorAll("#area").style.display="flex"  ;
+      // document.getElementById("#infoarea").style.display="display";
+      }
+      else{window.alert("Okay thanks for considering!")}
+      };
